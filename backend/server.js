38ad1c8a8e3950f50152a1a6f8caed6d47e52dbe
AbbasHjz1 <92792783+AbbasHjz1@users.noreply.github.com>
2022-10-03 00:32:01 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const path = require('path');
 
 connectDB()
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
