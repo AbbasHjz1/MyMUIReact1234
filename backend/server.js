a129dcb652be976b4238a1caff73b1ee987243d6
AbbasHjz1 <92792783+AbbasHjz1@users.noreply.github.com>
2022-10-02 23:58:20 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ app.use('/backend/item', require('./routes/itemRoute'))
 app.use('/backend/user', require('./routes/userRoute'))
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../client/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
 }
 else {
     app.get('/', (req,res) => res.send('Please set as a production'))
